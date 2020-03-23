a397e65c6f1597eb970b1987d6d4905a8b028260
Ash <valleyknight@protonmail.com>
2020-03-22 23:50:16 -0400

    tidy up some stuff, fix some stuff that was bothering me

--- FFNIFLFYTU.sh
+++ FFNIFLFYTU.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Pull most recent version of firefox from Mozilla.
+# Make sure user is not root.
+if [ "$(id -u)" = 0 ]; then
+	echo "Why are you running this as root?"
+	exit 1
+fi
+
+# Pull most recent version of Firefox Nightly from Mozilla.
 echo "downloading Firefox Nightly"
-wget -O ~/.FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US"
+wget "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US" -O ~/.FirefoxSetup.tar.bz2 
 read -p "Nightly Downloaded. Press [ENTER] to continue."
 
 # Extract the tar to "/opt".
-echo "Extracting tar to \"/opt\"."
+clear
+echo -e "Extracting tar to \"/opt\".\nIf you wish to inspect the files, they will be located in \"/opt/firefox\".\nPlease wait until you get the message stating that the tar is extracted."
 sudo tar xjf ~/.FirefoxSetup.tar.bz2 -C /opt/
-read -p  "Nightly Exctracted. Press [ENTER] to continue."
+read -p  "Nightly Extracted. Press [ENTER] to continue."
 
 # Ask for custom directory if wanted. If not wanted, link to "$HOME/bin".
-echo -e "Do you want to symlink to a custom directory, or just symlink to \"$HOME/bin\"?\nMake sure that whatever directory you choose is in your PATH.\n"
+clear
+echo -e "Do you want to symlink to a custom directory, or just symlink to \"$HOME/bin\"?\nMake sure that whatever directory you choose is in your \$PATH.\n"
 options=(
     "Custom Directory"
     "$HOME/bin"
@@ -22,15 +30,13 @@ select option in "${options[@]}"; do
             echo "Please input the full path of where you'd like to symlink. (Don't include the final slash)."
             read -r directory
             sudo ln -s /opt/firefox/firefox "$directory"/firefox-nightly
+			echo "Firefox executable has been symlinked to \"$directory/firefox-nightly\"."
             break
         ;;
         ${options[1]})
-            if test -d "$HOME"/bin; then
-				ln -s /opt/firefox/firefox "$HOME"/bin/firefox-nightly
-			else 
-				sudo mkdir -pv ~/bin
-				ln -s /opt/firefox/firefox "$HOME"/bin/firefox-nightly
-			fi
+            [ ! -d "$HOME/bin" ] && echo "Creating \"$HOME/bin\", make sure to add it to your \$PATH." && mkdir -pv "$HOME/bin"
+			ln -s /opt/firefox/firefox "$HOME"/bin/firefox-nightly
+			echo "Firefox executable has been symlinked to \"$HOME/bin/firefox-nightly\"."
 			break
         ;;
         *) echo "Invalid option.";;
@@ -38,7 +44,9 @@ select option in "${options[@]}"; do
 done
 
 # Remove the downloaded tar.
+clear
 echo "Removing tar."
 rm "$HOME"/.FirefoxSetup.tar.bz2
-read -p "tar removed. Press [ENTER] to continue."
+clear
+read "tar removed. Script is finished. You may use firefox-nightly now."
 exit 0
